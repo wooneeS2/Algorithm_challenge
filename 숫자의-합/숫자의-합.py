8d88816e66f1f0718a15e0478cbe73bb97255a06
KIM WONHEE <hello_world_@kakao.com>
2022-01-13 11:18:35 +0900

    Update 숫자의-합.py

--- 숫자의-합/숫자의-합.py
+++ 숫자의-합/숫자의-합.py
@@ -1,9 +1,12 @@
 import sys
 
 count = int(sys.stdin.readline())
+#rstrip()으로 뒤에 오는 공백 및 엔터 제거
 numbers = list(sys.stdin.readline().rstrip())
+#문자열을 리스트에 넣어주면 그 자체로 한글자씩 리스트가 생성됨
 
 sum = 0
 for i in numbers:
+    #문자열의 string을 int로 바꿔줌
     sum= sum+int(i)
 print(sum)
