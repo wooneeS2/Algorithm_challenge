9a39d6ed3f0695149df7fc41786b60e655358f6a
KIM WONHEE <hello_world_@kakao.com>
2022-01-13 11:44:31 +0900

    Update 알파벳-찾기.py

--- 알파벳-찾기/알파벳-찾기.py
+++ 알파벳-찾기/알파벳-찾기.py
@@ -1,11 +1,14 @@
 import sys
+#알파벳 리스트를 만들어주는 모듈임포트
 from string import ascii_lowercase
 
 word = sys.stdin.readline()
 alphabet = list(ascii_lowercase)
 result = []
 
+#js에서 indexOf역할을 하는 find
+# 첫번째로 찾은 위치를 반환하고, 없으면 -1 리턴
 for i in alphabet:
     result.append(word.find(i))
-    
+#리스트를 언팩킹하여 요구사항대로 출력    
 print(*result)
