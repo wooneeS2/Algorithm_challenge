e6fcb0e813a171c479ef9ce9379ede4b7da2b92a
KIM WONHEE <hello_world_@kakao.com>
2022-09-13 21:38:08 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,7 +1,9 @@
 function solution(n) {
-    let answer = [];
-    for(let i=0; i<n; i++){
-        i%2==0? answer.push('수'):answer.push('박')
-    }
-    return answer.join('')
+    // let answer = [];
+    // for(let i=0; i<n; i++){
+    //     i%2==0? answer.push('수'):answer.push('박')
+    // }
+    // return answer.join('')
+    let answer = '수박'.repeat(n).slice(0,n)
+    return answer
 }
