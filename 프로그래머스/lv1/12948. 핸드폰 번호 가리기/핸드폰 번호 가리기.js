14f4f8ee50b0116bc75dec2308e194acb095244c
KIM WONHEE <hello_world_@kakao.com>
2022-09-13 19:15:21 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,11 +1,6 @@
 function solution(phone_number) {
-    let number=phone_number.split('')
-    const hideNumber = [];
-    for(let i=0; i<phone_number.length-4; i++){
-        hideNumber.push('*')
-    }
-    for(let i=phone_number.length-4; i<phone_number.length; i++){
-        hideNumber.push(number[i])
-    }
-    return hideNumber.join("");
+
+        return '*'.repeat(phone_number.length-4) + phone_number.slice(phone_number.length-4)
+
+    
 }
