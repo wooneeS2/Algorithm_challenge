a7e3ec804d20abbec12f702e7c444d81488377a4
KIM WONHEE <hello_world_@kakao.com>
2022-09-13 21:31:01 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -1,11 +1,6 @@
 function solution(arr, divisor) {
     var answer = [];
-    for (let i=0; i<=arr.length;i++){
-        if(arr[i]%divisor===0){
-            answer.push(arr[i])
-        }
-        
-    }
+    answer = arr.filter(val=>val%divisor==0);
     if (answer.length===0){
         answer.push(-1)
     }
