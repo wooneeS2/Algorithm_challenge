5f388693d6bcee285d49f183bb125be875b21b9b
KIM WONHEE <hello_world_@kakao.com>
2022-09-14 12:48:14 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,12 @@
+function solution(price, money, count) {
+    let sum=0;
+    for(let i=1; i<=count; i++){
+        sum+=price*i
+    }
+    
+    if(sum-money<0){
+        return 0
+    }
+    
+    return sum-money;
+}
